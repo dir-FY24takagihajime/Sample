1d43088078371057ebdf5a578a1f126c41fbc6bd
h766hajimetakagi <hajime.takagi.h766@dirbato.co.jp>
2024-04-08 14:52:02 +0900

    Forbreak

--- Sample/src/sample/ForSample.java
+++ Sample/src/sample/ForSample.java
@@ -3,10 +3,10 @@ package sample;
 public class ForSample {
 
     public static void main(String[] args) {
-        for (var i = 1; i <= 20; i++) {
-            if (i % 2 != 0) {
-            //現在の処理をスキップし、次の繰り返しに進む
-                continue;
+        for (int i = 2; i <= 100; i+=2) {
+            if (i > 20) {
+            //繰り返しから抜け出す
+                break;
         }
         System.out.println(i);
         }

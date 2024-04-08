b1a6c6c30bd4460d23c81e77e122493c2050a650
h766hajimetakagi <hajime.takagi.h766@dirbato.co.jp>
2024-04-08 11:09:48 +0900

    Sample2-2

---
+++ Sample/src/sample/Test.java
@@ -0,0 +1,14 @@
+package sample;
+
+public class Test {
+    
+    //mainメソッドはプログラムの最初に起動します。
+    public static void main(String[] args) {
+        /* コメントアウト
+        System.out.println("kiramex");
+        System.out.println("Java");
+        */
+        System.out.println("学習を始めましょう！");
+    }
+
+}
